47377a7b0593a97440da16b901252962703e6344
jan-pfr <jp@janpf.de>
2020-11-21 12:10:18 +0100

    First steps.

---
+++ course/index.js
@@ -0,0 +1,10 @@
+//my first JavaScript code!
+console.log('Hello World');
+let name = 'Jan';
+let adresse = {
+  plz: 79787,
+  Ort: 'Lauchringen'
+}
+console.log(adresse);
+adresse.plz = 78120
+console.log(adresse.plz);
