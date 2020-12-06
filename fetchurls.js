6de445bc53cff6ca499ce156ac5eefc5322eed19
jan-pfr <jp@janpf.de>
2020-12-06 19:42:25 +0100

    fetchurls.js added.

--- fetchurls.js
+++ fetchurls.js
@@ -1,3 +1,10 @@
-fetch('http://localhost:3000/gettemperature/office').then(function (response){
-    console.log(response.json());
-});
+let id = 'bathroom';
+fetch('http://localhost:3000/gettemperature/'+ id)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+let degrees = 30;
+fetch('http://localhost:3000/settemperature/'+ id +'/'+ degrees)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
